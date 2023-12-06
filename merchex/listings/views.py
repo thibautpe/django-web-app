5414ef1f2675255d4c16bcf4948e30a5eb76b666
unknown <t.p@free.fr>
2023-12-06 16:07:24 +0100

    creation d'une première vue hello

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,7 @@
+# ~/projects/django-web-app/merchex/listings/views.py
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
