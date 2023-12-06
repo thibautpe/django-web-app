7397b9b8d7a7181f9645fb5ac6ace2da6e394ff2
unknown <t.p@free.fr>
2023-12-06 16:29:48 +0100

    creation 2ème vue about-us

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def hello(request):
     return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
